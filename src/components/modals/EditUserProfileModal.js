caae77f04ea2b33bda2113671c76439d680fdced
Zoraneko <tuandt1409@gmail.com>
2024-11-13 01:42:22 +0700

    Fix bug in Edit User Profile Modal
    Add logic to handleSaveChanges to ensure validity of updateBody.
    Update modal logic to ensure correct results for various actions.

--- src/components/modals/EditUserProfileModal.js
+++ src/components/modals/EditUserProfileModal.js
@@ -15,11 +15,25 @@ const EditUserProfileModal = ({ open, handleClose }) => {
 
   const [formData, setFormData] = useState({
     role: '',
-    identification: '',
-    phone: '',
-    city: '',
+    citizenId: '',
+    phoneNumber: '',
+    province: '',
     district: '',
-    ward: '',
+    town: '',
+    street: '',
+    isEmailVerified: false,
+    name: '',
+    email: '',
+    id: '',
+  });
+
+  const [tempForm, setTempForm] = useState({
+    role: '',
+    citizenId: '',
+    phoneNumber: '',
+    province: '',
+    district: '',
+    town: '',
     street: '',
     isEmailVerified: false,
     name: '',
@@ -28,69 +42,92 @@ const EditUserProfileModal = ({ open, handleClose }) => {
   });
 
   useEffect(() => {
-    if (open) {
-      setFormData({
+    if (open && user) {
+      const updatedFormData = {
         role: user?.role || '',
         isEmailVerified: user?.isEmailVerified || false,
         name: user?.name || '',
         email: user?.email || '',
-        phone: user?.phoneNumber || '',
-        identification: user?.citizenId || '',
-        city: user.address?.province || '',
+        phoneNumber: user?.phoneNumber || '',
+        citizenId: user?.citizenId || '',
+        province: user.address?.province || '',
         district: user.address?.district || '',
-        ward: user.address?.town || '',
+        town: user.address?.town || '',
         street: user.address?.street || '',
-      });
+      };
+
+      setFormData(updatedFormData);
+      setTempForm(updatedFormData);
     }
   }, [open, user]);
 
   const handleInputChange = (field, value) => {
-    setFormData((prev) => ({
+    setTempForm((prev) => ({
       ...prev,
       [field]: value,
     }));
   };
 
-  const isFormDataValid = ({ name, identification, email, phone, city, district, ward, street }) => {
-    
-    const validations = [
-      { valid: /^[A-Za-zÀ-ỹ\s]+$/.test(name), message: 'Vui lòng nhập Họ tên hợp lệ' },
-      { valid: /^[0-9]{9}$|^[0-9]{12}$/.test(identification), message: 'Vui lòng nhập đúng số CCCD' },
-      { valid: /^\+?[0-9]{10,15}$/.test(phone), message: 'Vui lòng nhập đúng Số điện thoại' },
-      {
-        valid: /^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$/.test(email),
-        message: 'Vui lòng nhập email hợp lệ',
-      },
-      { valid: city && district && ward && street, message: 'Vui lòng điền đầy đủ thông tin địa chỉ' },
-    ];
+  const isFormDataValid = ({ name, citizenId, email, phoneNumber, province, district, town, street }) => {
+    if (!name || !/^[A-Za-zÀ-ỹ\s]+$/.test(name)) {
+      toast.error('Vui lòng nhập Họ tên hợp lệ');
+      return false;
+    }
 
-    for (let { valid, message } of validations) {
-      if (!valid) {
-        toast.error(message);
-        return false;
-      }
+    if (!email || !/^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$/.test(email)) {
+      toast.error('Vui lòng nhập email hợp lệ');
+      return false;
+    }
+    if (citizenId && !/^[0-9]{9}$|^[0-9]{12}$/.test(citizenId)) {
+      toast.error('Vui lòng nhập đúng số CCCD');
+      return false;
+    }
+
+    if (phoneNumber && !/^\+?[0-9]{10,15}$/.test(phoneNumber)) {
+      toast.error('Vui lòng nhập đúng Số điện thoại');
+      return false;
     }
 
     return true;
   };
 
   const handleSaveChanges = async () => {
-    if (!isFormDataValid(formData)) {
+    if (!isFormDataValid(tempForm)) {
     } else {
-      const updateBody = {
-        phoneNumber: formData.phone,
-        name: formData.name,
-        email: formData.email,
-        address: {
-          province: formData.city,
-          district: formData.district,
-          town: formData.ward,
-          street: formData.street,
-        },
-      };
+      const updateBody = {};
+
+      Object.entries(tempForm).forEach(([key, value]) => {
+        if (
+          key !== 'isEmailVerified' &&
+          key !== 'id' &&
+          key !== 'role' &&
+          key !== 'province' &&
+          key !== 'district' &&
+          key !== 'town' &&
+          key !== 'street'
+        ) {
+          if (value !== '') {
+            updateBody[key] = value;
+          }
+        }
+      });
+
+      const address = {};
+      if (tempForm.province !== '') address.province = tempForm.province || null;
+      if (tempForm.district !== '') address.district = tempForm.district || null;
+      if (tempForm.town !== '') address.town = tempForm.town || null;
+      if (tempForm.street !== '') address.street = tempForm.street || null;
+
+      if (Object.values(address).some((field) => field !== null)) {
+        updateBody.address = address;
+      }
+
+      console.log(updateBody);
+
       dispatch(updateUser({ id: user.id, updatedUserInfo: updateBody }))
         .unwrap()
         .then(() => {
+          setFormData(tempForm);
           toast.success('Cập nhật thông tin thành công');
         })
         .catch(() => {
@@ -169,22 +206,27 @@ const EditUserProfileModal = ({ open, handleClose }) => {
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
             <LabeledTextField
               label="Họ và tên"
-              value={formData.name}
+              value={tempForm.name}
               onChange={(value) => handleInputChange('name', value)}
             />
             <LabeledTextField
               label="CMND/CCCD"
-              value={formData.identification}
-              onChange={(value) => handleInputChange('identification', value)}
+              value={tempForm.citizenId}
+              onChange={(value) => handleInputChange('citizenId', value)}
             />
           </Box>
 
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
-            <LabeledTextField label="Email" value={formData.email} onChange={(value) => handleInputChange('email', value)} />
+            <LabeledTextField
+              label="Email"
+              value={tempForm.email}
+              onChange={(value) => handleInputChange('email', value)}
+              disabled={tempForm.isEmailVerified}
+            />
             <LabeledTextField
               label="Số điện thoại"
-              value={formData.phone}
-              onChange={(value) => handleInputChange('phone', value)}
+              value={tempForm.phoneNumber}
+              onChange={(value) => handleInputChange('phoneNumber', value)}
             />
           </Box>
         </Box>
@@ -210,18 +252,18 @@ const EditUserProfileModal = ({ open, handleClose }) => {
           </Typography>
 
           <ProvinceSelector
-            city={formData.city}
-            district={formData.district}
-            ward={formData.ward}
-            onCityChange={(value) => handleInputChange('city', value)}
+            city={tempForm.province}
+            district={tempForm.district}
+            ward={tempForm.town}
+            onCityChange={(value) => handleInputChange('province', value)}
             onDistrictChange={(value) => handleInputChange('district', value)}
-            onWardChange={(value) => handleInputChange('ward', value)}
+            onWardChange={(value) => handleInputChange('town', value)}
           />
 
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
             <LabeledTextField
               label="Số nhà, đường/phố"
-              value={formData?.street || ''}
+              value={tempForm?.street || ''}
               onChange={(value) => handleInputChange('street', value)}
             />
           </Box>
