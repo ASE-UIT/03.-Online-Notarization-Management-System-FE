b28f87672db06be33b77f7e4afb6cb80722871ab
not-thanhtai <tai606760@gmail.com>
2024-12-16 14:49:21 +0700

    Add custom hooks for window size and event listener; refactor Notary components for improved layout

--- src/components/notary/InformationField.js
+++ src/components/notary/InformationField.js
@@ -3,16 +3,12 @@ import { Box, Typography } from '@mui/material';
 import { black } from '../../config/theme/themePrimitives';
 
 const InformationField = ({ title, value }) => {
-    return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
-            <Typography sx={{ fontSize: 12, fontWeight: 600, color: black[900], textAlign: 'left' }}>
-                {title}:
-            </Typography>
-            <Typography sx={{ flex: 1, fontSize: 12, fontWeight: 400, color: black[500], textAlign: 'right' }}>
-                {value}
-            </Typography>
-        </Box>
-    );
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', gap: 1 }}>
+      <Typography sx={{ fontSize: 12, fontWeight: 600, color: black[900], textAlign: 'left' }}>{title}:</Typography>
+      <Typography sx={{ flex: 1, fontSize: 12, fontWeight: 400, color: black[500], textAlign: 'right' }}>{value}</Typography>
+    </Box>
+  );
 };
 
 export default InformationField;
